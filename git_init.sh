5fff64b8113ac31e15c541858299f7ca30f71ba4
LZA <318724186@qq.com>
2018-04-28 01:58:49 +0800

    2018-04-28

---
+++ git_init.sh
@@ -0,0 +1,7 @@
+git config --global user.name "LZA"
+git config --global user.email "318724186@qq.com"
+
+git init
+#git remote add origin git@github.com:michaelliao/learngit.git
+git remote add dockers  git@github.com:zhengjue/companyDockers.git
+#git@github.com:zhengjue/scripts.git
