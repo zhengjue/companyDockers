437f571b1bd9ff575a1748166c013531c78e4094
LZA <318724186@qq.com>
2018-04-28 17:45:05 +0800

    2018-04-28

--- git_commit.sh
+++ git_commit.sh
@@ -1,7 +1,4 @@
 git add --all
 git commit -m "$(date +%F)"
-<<<<<<< HEAD
 git push dockers master
-=======
-git push origin master
->>>>>>> 5fff64b8113ac31e15c541858299f7ca30f71ba4
+#git push origin master
